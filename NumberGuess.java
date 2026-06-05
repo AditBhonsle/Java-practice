3c597a6ff88c9b9e37ba90e2c73670b19d2ec775
Adit <aditbhonsle@gmail.com>
2026-06-06 00:18:02 +0530

    V1.5: Add attempt limit and improve game messages

--- NumberGuess.java
+++ NumberGuess.java
@@ -5,25 +5,36 @@ public class NumberGuess
     Scanner s=new Scanner(System.in);
     void game()
     {
-        int guess;
+        int guess,attempt=7;
         int num=rand.nextInt(100)+1;
-        while(true)
+        System.out.println("Welcome to the Number guess game!");
+        System.out.println("You have to guess a number between 1 and 100 in 7 attempts!");
+        while(attempt>0)
         {
-            System.out.println("Enter your guess");
+            System.out.println();
+            System.out.println("Enter your guess ("+attempt+" attempts left!)");
             guess=s.nextInt();
             if(guess>num)
             {
-                System.out.println("Guess too high!");
+                System.out.println("Guess is too high!");
+                attempt--;
             }
             else if(guess<num)
             {
-                System.out.println("Guess too low!");
+                System.out.println("Guess is too low!");
+                attempt--;
             }
             else 
             {
                 System.out.println("Correct!");
+                System.out.println("You guessed the number in "+(8-attempt)+" attempts");
                 break;
             }
+        }
+        if(attempt==0)
+        {
+            System.out.println("Game over :(");
+            System.out.println("The number was "+num);
         }
     }
     public static void main()
