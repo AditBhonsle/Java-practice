88c6cd66a66d7edb32c0c9964e0db4fe75b84860
Adit <aditbhonsle@gmail.com>
2026-06-07 23:30:39 +0530

    V1.1: Refactor calculator code for readability

--- Calculator.java
+++ Calculator.java
@@ -4,66 +4,91 @@ public class Calculator
     char op;                        // Op=operation
     int num1,num2;
     Scanner s=new Scanner(System.in);
+    void display()
+    {
+        System.out.println();
+        System.out.println("Welcome to Calculator");
+        System.out.println("Choose any of the following operations!");
+        System.out.println("+ (Addition)");
+        System.out.println("- (Subtraction)");
+        System.out.println("* (Multiplication)");
+        System.out.println("/ (Division)");
+        System.out.println("% (Remainder)");
+        System.out.println("Q (Exit the calculator)");
+    }
+
+    void accept()
+    {
+        System.out.println("Enter number 1");
+        num1=s.nextInt();
+        System.out.println("Enter number 2");
+        num2=s.nextInt();
+    }
+
+    void solve()
+    {
+        switch(op)
+        {
+            case '+':
+                System.out.println("Result = "+(num1+num2));
+                break;
+            case '-':
+                System.out.println("Result = "+(num1-num2));
+                break;
+            case '*':
+                System.out.println("Result = "+(num1*num2));
+                break;
+            case '/':
+                System.out.println("Result = "+(num1/num2));
+                break;
+            case '%':
+                System.out.println("Result = "+(num1%num2));
+                break;
+        }
+    }
+
+    boolean validOperation()
+    {
+        return op=='+' ||
+        op=='-' ||
+        op=='*' ||
+        op=='/' ||
+        op=='%';
+    }
+
     void calculate()
     {
         while(true)
         {
-            System.out.println();
-            System.out.println("Welcome to Calculator");
-            System.out.println("Choose any of the following operations!");
-            System.out.println("+ (Addition)");
-            System.out.println("- (Subtraction)");
-            System.out.println("* (Multiplication)");
-            System.out.println("/ (Division)");
-            System.out.println("% (Remainder)");
-            System.out.println("Q (Exit the calculator)");
-            op=s.next().charAt(0);
-            if(op=='Q' || op=='q')
+            display();
+            op=s.next().toLowerCase().charAt(0);
+            if(op=='q')
             {
                 System.out.println("Thanks for using the calculator!");
                 break;
             }
             else
             {
-                if(op!='+' && op!='-' && op!='*' && op!='/' && op!='%')
+                if(!validOperation())
                 {
                     System.out.println("Invalid operation");
                 }
                 else
                 {
-                    System.out.println("Enter number 1");
-                    num1=s.nextInt();
-                    System.out.println("Enter number 2");
-                    num2=s.nextInt();
+                    accept();
                     if(num2==0 && (op=='/' || op=='%'))
                     {
                         System.out.println("Can't divide by zero");
                     }
                     else
                     {
-                        switch(op)
-                        {
-                            case '+':
-                                System.out.println("Result = "+(num1+num2));
-                                break;
-                            case '-':
-                                System.out.println("Result = "+(num1-num2));
-                                break;
-                            case '*':
-                                System.out.println("Result = "+(num1*num2));
-                                break;
-                            case '/':
-                                System.out.println("Result = "+(num1/num2));
-                                break;
-                            default:
-                                System.out.println("Result = "+(num1%num2));
-                                break;
-                        }
+                        solve();
                     }
                 }
             }
         }
     }
+
     public static void main()
     {
         Calculator c=new Calculator();
